9e2001f81836b6156337d622b09c741759a76316
cheriassie <cheriassie@gmail.com>
2024-11-02 23:18:00 +0100

    Product Types added

---
+++ src/types/ProductTypes.ts
@@ -0,0 +1,15 @@
+export interface ProductTypes {
+    id: number;
+    brand: string;                
+    model: string;                
+    releaseYear: number;
+    screenSize: number;
+    countryOfOrigin: string;
+    memory: number;
+    refreshRate: number;
+    nfc: boolean;
+    esim: boolean;
+    wirelessCharging: boolean;
+    price: number;
+  }
+  
